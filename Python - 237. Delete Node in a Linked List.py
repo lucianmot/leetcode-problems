9017699932a5721583216cf5f57c76fd05624903
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-13 09:31:21 +0000

    Create Python - 237. Delete Node in a Linked List.py

---
+++ Python - 237. Delete Node in a Linked List.py
@@ -0,0 +1,7 @@
+# Python - 237. Delete Node in a Linked List
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
